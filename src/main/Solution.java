29c8db08e23c4c32a7621dfd04220e234658b816
dorianHGit <thepictueofdoriansinbox@outlook.com>
2020-02-11 16:46:08 +0000

    Moved Initial algorithm constraints to separate method. Ensured Tests Still pass Next: Implement Rotation algorithm

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -5,6 +5,11 @@ public class Solution {
     public Solution(){}
 
     public int[] rotLeft(int[] a, int d) {
+        constraintCheck(a, d);
+        return a;
+    }
+
+    private void constraintCheck(int[] a, int d){
         if(a.length < 1){
             throw new IllegalArgumentException("You cannot submit an empty array");
         }
@@ -17,7 +22,6 @@ public class Solution {
         if(d > a.length){
             throw new IllegalArgumentException("You cannot perform more rotations than that equivalent to the length of the array.");
         }
-        return a;
     }
 
 }
