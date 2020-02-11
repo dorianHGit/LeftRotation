49c3f84300958ce4e15cef3b51df63932a6d8a37
dorianHGit <thepictueofdoriansinbox@outlook.com>
2020-02-11 15:45:16 +0000

    Added Sample Test cases from hackerrank to test suite. Next: Add constraint tests

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,4 +1,11 @@
 package main;
 
 public class Solution {
+
+    public Solution(){}
+
+    public int[] rotLeft(int[] a, int d) {
+        return a;
+    }
+
 }
--- src/test/TestSolution.java
+++ src/test/TestSolution.java
@@ -1,6 +1,37 @@
 package test;
 
+import main.Solution;
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestSolution {
+
+    private Solution solution;
+
+    @Before
+    public void setUp(){
+       solution = new Solution();
+    }
+
+    @Test
+    public void testCaseArrayOfFiveFourRotations(){
+        int[] expectedArray = {5,1,2,3,4};
+        int[] inputArray = {1,2,3,4,5};
+        assertEquals(expectedArray, solution.rotLeft(inputArray, 4));
+    }
+
+    @Test
+    public void testCaseArrayOfTwentyTenRotations(){
+        int[] expectedArray = {77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77};
+        int[] inputArray = {41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51};
+        assertEquals(expectedArray, solution.rotLeft(inputArray, 10));
+    }
+
+    @Test
+    public void testCaseArrayOfFifteenThirteenRotations(){
+        int[] expectedArray = {87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60};
+        int[] inputArray = {33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97};
+        assertEquals(expectedArray, solution.rotLeft(inputArray, 13));
+    }
 }
