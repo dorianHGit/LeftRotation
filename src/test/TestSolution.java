4e47026fa42f6c89f45f0d46c4e309297f634b1e
dorianHGit <thepictueofdoriansinbox@outlook.com>
2020-02-11 16:05:07 +0000

    Added constraint tests beyond the limits. Next: Add constraint tests just within the limits

--- src/test/TestSolution.java
+++ src/test/TestSolution.java
@@ -2,6 +2,7 @@ package test;
 
 import main.Solution;
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,6 +14,9 @@ public class TestSolution {
     public void setUp(){
        solution = new Solution();
     }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void testCaseArrayOfFiveFourRotations(){
@@ -33,5 +37,51 @@ public class TestSolution {
         int[] expectedArray = {87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60};
         int[] inputArray = {33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97};
         assertEquals(expectedArray, solution.rotLeft(inputArray, 13));
+    }
+
+    @Test
+    public void assertEmptyArrayThrowsException(){
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("You cannot submit an empty array");
+        int[] inputArray = new int[]{};
+    }
+
+    @Test
+    public void assertArrayLargerThan10pow5ThrowsException(){
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Submitted arrays must be smaller than 10 to the power of 5.");
+        int[] inputArray = new int[(int)Math.pow(10,5)+1];
+    }
+
+    @Test
+    public void assertRotationLessThanOneThrowsException(){
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("You cannot perform less than one rotation.");
+        int[] inputArray = {1,2,3,4,5};
+        solution.rotLeft(inputArray, 0);
+    }
+
+    @Test
+    public void assertRotationGreaterThanLengthThrowsException(){
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("You cannot perform more rotations than that equivalent to the length of the array.");
+        int[] inputArray = {1,2,3,4,5};
+        solution.rotLeft(inputArray, 6);
+    }
+
+    @Test
+    public void assertArrayItemLessThanOneThrowsException(){
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Items in the array must be integers between 1 and 10^6");
+        int[] inputArray = {0,2,3,4,5};
+        solution.rotLeft(inputArray, 4);
+    }
+
+    @Test
+    public void assertArrayItemGreaterThan10pow6ThrowsException(){
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Items in the array must be integers between 1 and 10^6");
+        int[] inputArray = {(int)Math.pow(10,6)+1,2,3,4,5};
+        solution.rotLeft(inputArray, 4);
     }
 }
